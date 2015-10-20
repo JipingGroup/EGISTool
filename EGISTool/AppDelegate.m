471a281677d57782a5eaaf569b3f7427f0e63eae
CommonTan <1015658706@qq.com>
2015-10-20 14:23:21 +0800

    写了注视

--- EGISTool/AppDelegate.m
+++ EGISTool/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //
+    //it is a test
     return YES;
 }
 
